DrexHD/Vanish

commit f0c8db9f924d07aa2f9c1605fbb437c6d284ecfc

change: Replace streams with for loops

src/main/java/me/drex/vanish/mixin/ServerGamePacketListenerImplMixin.java
@@ -2,6 +2,7 @@ package me.drex.vanish.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import me.drex.vanish.api.VanishAPI;
 import net.minecraft.network.PacketSendListener;
 import net.minecraft.network.chat.Component;
@@ -21,9 +22,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.List;
-import java.util.Objects;
 
 @Mixin(ServerGamePacketListenerImpl.class)
 public abstract class ServerGamePacketListenerImplMixin {
@@ -51,10 +49,16 @@ public abstract class ServerGamePacketListenerImplMixin {
                 ci.cancel();
             }
         } else if (packet instanceof ClientboundPlayerInfoPacket playerInfoPacket) {
-            List<ClientboundPlayerInfoPacket.PlayerUpdate> modified = playerInfoPacket.getEntries().stream().filter(playerUpdate -> VanishAPI.canSeePlayer(server, playerUpdate.getProfile().getId(), this.player)).toList();
-            if (modified.size() != playerInfoPacket.getEntries().size()) {
-                if (!modified.isEmpty()) {
-                    this.send(new ClientboundPlayerInfoPacket(playerInfoPacket.getAction(), modified.stream().map(playerUpdate -> this.server.getPlayerList().getPlayer(playerUpdate.getProfile().getId())).filter(Objects::nonNull).toList()));
+            ObjectArrayList<ServerPlayer> modifiedEntries = new ObjectArrayList<>();
+            for (ClientboundPlayerInfoPacket.PlayerUpdate playerUpdate : playerInfoPacket.getEntries()) {
+                if (VanishAPI.canSeePlayer(server, playerUpdate.getProfile().getId(), this.player)) {
+                    ServerPlayer player = server.getPlayerList().getPlayer(playerUpdate.getProfile().getId());
+                    if (player != null) modifiedEntries.add(player);
+                }
+            }
+            if (modifiedEntries.size() != playerInfoPacket.getEntries().size()) {
+                if (!modifiedEntries.isEmpty()) {
+                    this.send(new ClientboundPlayerInfoPacket(playerInfoPacket.getAction(), modifiedEntries));
                 }
                 ci.cancel();
             }

src/main/java/me/drex/vanish/api/VanishAPI.java
@@ -1,5 +1,6 @@
 package me.drex.vanish.api;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import me.drex.vanish.util.VanishManager;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
@@ -72,10 +73,13 @@ public interface VanishAPI {
     @NotNull
     static List<ServerPlayer> getVisiblePlayers(@NotNull CommandSourceStack source) {
         MinecraftServer server = source.getServer();
-        return server.getPlayerList().getPlayers()
-                .stream()
-                .filter(serverPlayer -> canSeePlayer(server, serverPlayer.getUUID(), source))
-                .toList();
+        ObjectArrayList<ServerPlayer> list = new ObjectArrayList<>();
+        for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+            if (canSeePlayer(server, player.getUUID(), source)) {
+                list.add(player);
+            }
+        }
+        return list;
     }
 
     /**
@@ -86,8 +90,13 @@ public interface VanishAPI {
      */
     @NotNull
     static List<ServerPlayer> getViewingPlayers(@NotNull ServerPlayer player) {
-        return player.server.getPlayerList().getPlayers().stream()
-                .filter(viewer -> canSeePlayer(player, viewer)).toList();
+        ObjectArrayList<ServerPlayer> list = new ObjectArrayList<>();
+        for (ServerPlayer viewer : player.server.getPlayerList().getPlayers()) {
+            if (canSeePlayer(player, viewer)) {
+                list.add(viewer);
+            }
+        }
+        return list;
     }
 
     /**

src/main/java/me/drex/vanish/util/VanishManager.java
@@ -27,9 +27,13 @@ public class VanishManager {
 
     public static void init() {
         ServerTickEvents.START_SERVER_TICK.register(server -> {
-            if (ConfigManager.INSTANCE.vanish().actionBar)
-                server.getPlayerList().getPlayers().stream().filter(VanishAPI::isVanished)
-                        .forEach(serverPlayer -> serverPlayer.sendSystemMessage(Component.translatable("text.vanish.general.vanished"), true));
+            if (ConfigManager.INSTANCE.vanish().actionBar) {
+                for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+                    if (VanishAPI.isVanished(player)) {
+                        player.sendSystemMessage(Component.translatable("text.vanish.general.vanished"), true);
+                    }
+                }
+            }
         });
         ServerMessageEvents.ALLOW_CHAT_MESSAGE.register((message, sender, params) -> {
             if (VanishAPI.isVanished(sender) && ConfigManager.INSTANCE.vanish().disableChat) {
@@ -104,11 +108,11 @@ public class VanishManager {
     }
 
     private static void broadcastToOthers(ServerPlayer vanisher, Packet<?> packet) {
-        vanisher.server.getPlayerList().getPlayers().stream()
-                .filter(viewer -> !canViewVanished(viewer) && !viewer.equals(vanisher))
-                .forEach(viewer -> {
-                    viewer.connection.send(packet);
-                });
+        for (ServerPlayer viewer : vanisher.server.getPlayerList().getPlayers()) {
+            if (!canViewVanished(viewer) && !viewer.equals(vanisher)) {
+                viewer.connection.send(packet);
+            }
+        }
     }
 
 }

commit 09960710794dc8d785510d17bf8f17d16d4ca6a0

fix: Remove player action not being sent
This issue was introduced in f0c8db9f924d07aa2f9c1605fbb437c6d284ecfc

src/main/java/me/drex/vanish/mixin/ServerGamePacketListenerImplMixin.java
@@ -50,13 +50,15 @@ public abstract class ServerGamePacketListenerImplMixin {
             }
         } else if (packet instanceof ClientboundPlayerInfoPacket playerInfoPacket) {
             ObjectArrayList<ServerPlayer> modifiedEntries = new ObjectArrayList<>();
+            int visible = 0;
             for (ClientboundPlayerInfoPacket.PlayerUpdate playerUpdate : playerInfoPacket.getEntries()) {
                 if (VanishAPI.canSeePlayer(server, playerUpdate.getProfile().getId(), this.player)) {
+                    visible++;
                     ServerPlayer player = server.getPlayerList().getPlayer(playerUpdate.getProfile().getId());
                     if (player != null) modifiedEntries.add(player);
                 }
             }
-            if (modifiedEntries.size() != playerInfoPacket.getEntries().size()) {
+            if (visible != playerInfoPacket.getEntries().size()) {
                 if (!modifiedEntries.isEmpty()) {
                     this.send(new ClientboundPlayerInfoPacket(playerInfoPacket.getAction(), modifiedEntries));
                 }