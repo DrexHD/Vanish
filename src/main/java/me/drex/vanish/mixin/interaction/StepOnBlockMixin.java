cdfeb96f739bce536760573577d5cc5b5ee0c633
Drex <nicknamedrex@gmail.com>
2023-03-07 11:39:37 +0100

    add: Prevent player step on block interactions
    Prevents:
    - Farmland trampling
    - Turtleegg trampling
    - Redstone ore activating on step on (server side)
    - Sculk sensor activating on step on
    - Sculk shrieker activating on step on

---
+++ src/main/java/me/drex/vanish/mixin/interaction/StepOnBlockMixin.java
@@ -0,0 +1,25 @@
+package me.drex.vanish.mixin.interaction;
+
+import me.drex.vanish.api.VanishAPI;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.RedStoneOreBlock;
+import net.minecraft.world.level.block.SculkSensorBlock;
+import net.minecraft.world.level.block.SculkShriekerBlock;
+import net.minecraft.world.level.block.TurtleEggBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = {RedStoneOreBlock.class, SculkSensorBlock.class, SculkShriekerBlock.class, TurtleEggBlock.class})
+public abstract class StepOnBlockMixin {
+
+    @Inject(method = "stepOn", at = @At("HEAD"), cancellable = true)
+    private void vanish_cancelEntityStepOnBlock(Level level, BlockPos blockPos, BlockState blockState, Entity entity, CallbackInfo ci) {
+        if (VanishAPI.isVanished(entity)) ci.cancel();
+    }
+
+}
