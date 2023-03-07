2786924976e3ae7ae3ea8ceb5e932dbad1cd0514
Drex <nicknamedrex@gmail.com>
2023-03-07 02:24:41 +0100

    add: Prevent player inside block interactions
    Hides:
    - Pressure plates triggering
    - Dripleafs triggering
    - Tripwires triggering

---
+++ src/main/java/me/drex/vanish/mixin/interaction/InsideBlockMixin.java
@@ -0,0 +1,22 @@
+package me.drex.vanish.mixin.interaction;
+
+import me.drex.vanish.api.VanishAPI;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = {BasePressurePlateBlock.class, BigDripleafBlock.class, CropBlock.class, TripWireBlock.class})
+public abstract class InsideBlockMixin {
+
+    @Inject(method = "entityInside", at = @At("HEAD"), cancellable = true)
+    private void vanish_cancelEntityInsideBlock(BlockState blockState, Level level, BlockPos blockPos, Entity entity, CallbackInfo ci) {
+        if (VanishAPI.isVanished(entity)) ci.cancel();
+    }
+
+}
