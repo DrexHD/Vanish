02880e83334d75b3e71d79fd4e610a9e40070410
Drex <nicknamedrex@gmail.com>
2023-03-08 01:12:00 +0100

    add: Prevent entity collisions

---
+++ src/main/java/me/drex/vanish/mixin/interaction/EntitySelectorMixin.java
@@ -0,0 +1,20 @@
+package me.drex.vanish.mixin.interaction;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import me.drex.vanish.api.VanishAPI;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntitySelector;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.function.Predicate;
+
+@Mixin(EntitySelector.class)
+public abstract class EntitySelectorMixin {
+
+    @ModifyReturnValue(method = "pushableBy", at = @At("RETURN"))
+    private static Predicate<Entity> vanish_preventEntityCollision(Predicate<Entity> original, Entity pushableBy) {
+        return original.and(entity -> !VanishAPI.isVanished(entity)).and(entity -> !VanishAPI.isVanished(pushableBy));
+    }
+
+}
