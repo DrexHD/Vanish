d9511b82a6a8ef01245bc04f9002af9e235cbead
Drex <nicknamedrex@gmail.com>
2023-08-07 16:50:27 +0200

    Update to 23w31a

---
+++ src/main/java/me/drex/vanish/mixin/ServerCommonPacketListenerImplMixin.java
@@ -0,0 +1,64 @@
+package me.drex.vanish.mixin;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import me.drex.vanish.api.VanishAPI;
+import net.minecraft.network.PacketSendListener;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
+import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket;
+import net.minecraft.network.protocol.game.ClientboundTakeItemEntityPacket;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerCommonPacketListenerImpl;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.entity.Entity;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerCommonPacketListenerImpl.class)
+public abstract class ServerCommonPacketListenerImplMixin {
+
+    @Shadow @Final protected MinecraftServer server;
+
+    @Shadow public abstract void send(Packet<?> packet);
+
+    @Inject(
+        method = "send(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketSendListener;Z)V",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    public void vanish_modifyPackets(Packet<?> packet, @Nullable PacketSendListener packetSendListener, boolean flush, CallbackInfo ci) {
+        if ((Object) this instanceof ServerGamePacketListenerImpl listener) {
+            if (packet instanceof ClientboundTakeItemEntityPacket takeItemEntityPacket) {
+                Entity entity = listener.player.level().getEntity(takeItemEntityPacket.getPlayerId());
+                if (entity instanceof ServerPlayer actor && !VanishAPI.canSeePlayer(actor, listener.player)) {
+                    this.send(new ClientboundRemoveEntitiesPacket(takeItemEntityPacket.getItemId()));
+                    ci.cancel();
+                }
+            } else if (packet instanceof ClientboundPlayerInfoUpdatePacket playerInfoPacket) {
+                ObjectArrayList<ServerPlayer> modifiedEntries = new ObjectArrayList<>();
+                int visible = 0;
+                for (ClientboundPlayerInfoUpdatePacket.Entry playerUpdate : playerInfoPacket.entries()) {
+                    if (VanishAPI.canSeePlayer(server, playerUpdate.profileId(), listener.player)) {
+                        visible++;
+                        ServerPlayer player = server.getPlayerList().getPlayer(playerUpdate.profileId());
+                        if (player != null) modifiedEntries.add(player);
+                    }
+                }
+                if (visible != playerInfoPacket.entries().size()) {
+                    if (!modifiedEntries.isEmpty()) {
+                        this.send(new ClientboundPlayerInfoUpdatePacket(playerInfoPacket.actions(), modifiedEntries));
+                    }
+                    ci.cancel();
+                }
+            }
+        }
+    }
+
+
+}
--- src/main/java/me/drex/vanish/mixin/ServerGamePacketListenerImplMixin.java
+++ src/main/java/me/drex/vanish/mixin/ServerGamePacketListenerImplMixin.java
@@ -2,26 +2,14 @@ package me.drex.vanish.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import me.drex.vanish.api.VanishAPI;
-import net.minecraft.network.PacketSendListener;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
-import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket;
-import net.minecraft.network.protocol.game.ClientboundTakeItemEntityPacket;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.server.players.PlayerList;
-import net.minecraft.world.entity.Entity;
-import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerGamePacketListenerImpl.class)
 public abstract class ServerGamePacketListenerImplMixin {
@@ -29,46 +17,8 @@ public abstract class ServerGamePacketListenerImplMixin {
     @Shadow
     public ServerPlayer player;
 
-    @Shadow
-    public abstract void send(Packet<?> packet);
-
-    @Shadow
-    @Final
-    private MinecraftServer server;
-
-    @Inject(
-            method = "send(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketSendListener;)V",
-            at = @At("HEAD"),
-            cancellable = true
-    )
-    public void vanish_modifyPackets(Packet<?> packet, @Nullable PacketSendListener packetSendListener, CallbackInfo ci) {
-        if (packet instanceof ClientboundTakeItemEntityPacket takeItemEntityPacket) {
-            Entity entity = this.player.level().getEntity(takeItemEntityPacket.getPlayerId());
-            if (entity instanceof ServerPlayer actor && !VanishAPI.canSeePlayer(actor, this.player)) {
-                this.send(new ClientboundRemoveEntitiesPacket(takeItemEntityPacket.getItemId()));
-                ci.cancel();
-            }
-        } else if (packet instanceof ClientboundPlayerInfoUpdatePacket playerInfoPacket) {
-            ObjectArrayList<ServerPlayer> modifiedEntries = new ObjectArrayList<>();
-            int visible = 0;
-            for (ClientboundPlayerInfoUpdatePacket.Entry playerUpdate : playerInfoPacket.entries()) {
-                if (VanishAPI.canSeePlayer(server, playerUpdate.profileId(), this.player)) {
-                    visible++;
-                    ServerPlayer player = server.getPlayerList().getPlayer(playerUpdate.profileId());
-                    if (player != null) modifiedEntries.add(player);
-                }
-            }
-            if (visible != playerInfoPacket.entries().size()) {
-                if (!modifiedEntries.isEmpty()) {
-                    this.send(new ClientboundPlayerInfoUpdatePacket(playerInfoPacket.actions(), modifiedEntries));
-                }
-                ci.cancel();
-            }
-        }
-    }
-
     @WrapOperation(
-            method = "onDisconnect",
+            method = "removePlayerFromWorld",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/server/players/PlayerList;broadcastSystemMessage(Lnet/minecraft/network/chat/Component;Z)V"
