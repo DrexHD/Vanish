a3b5e2186c4daacb92ff0f975455c0fad3e1cc30
Drex <nicknamedrex@gmail.com>
2023-11-08 00:38:04 +0100

    Fix polydex tooltips of vanished players
    fixes Patbox/polydex#10

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Projectiles colliding/hitting players
 - Boat and Minecart collisions
 - Queries showing actual player count
+- Polydex tooltips of vanished players
 
 ## [1.4.3] - 2023-10-04
 ### Added
--- src/main/java/me/drex/vanish/mixin/EntityMixin.java
+++ src/main/java/me/drex/vanish/mixin/EntityMixin.java
@@ -4,6 +4,7 @@ import me.drex.vanish.api.VanishAPI;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.TraceableEntity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -25,6 +26,14 @@ public abstract class EntityMixin {
         }
         if (!VanishAPI.canSeePlayer(actor, observer)) {
             cir.setReturnValue(false);
+        }
+    }
+
+    @Inject(method = "isInvisibleTo", at = @At("HEAD"), cancellable = true)
+    private void vanish_markRenderInvisible(Player player, CallbackInfoReturnable<Boolean> cir) {
+        Entity self = (Entity) (Object) this;
+        if (self instanceof ServerPlayer actor && player instanceof ServerPlayer observer && !VanishAPI.canSeePlayer(actor, observer)) {
+            cir.setReturnValue(true);
         }
     }
 
