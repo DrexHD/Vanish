5c1c56e6918972daa03ba82cf2fe9312fb84b132
Drex <nicknamedrex@gmail.com>
2023-12-18 22:00:14 +0100

    Update to 23w51b

--- src/main/java/me/drex/vanish/mixin/interaction/VibrationSystemMixin.java
+++ src/main/java/me/drex/vanish/mixin/interaction/VibrationSystemMixin.java
@@ -2,6 +2,7 @@ package me.drex.vanish.mixin.interaction;
 
 import me.drex.vanish.api.VanishAPI;
 import me.drex.vanish.config.ConfigManager;
+import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -16,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class VibrationSystemMixin {
 
     @Inject(method = "handleGameEvent", at = @At("HEAD"), cancellable = true)
-    private void vanish_preventEntityVibrations(ServerLevel serverLevel, GameEvent gameEvent, GameEvent.Context context, Vec3 vec3, CallbackInfoReturnable<Boolean> cir) {
+    private void vanish_preventEntityVibrations(ServerLevel serverLevel, Holder<GameEvent> holder, GameEvent.Context context, Vec3 vec3, CallbackInfoReturnable<Boolean> cir) {
         Entity sourceEntity = context.sourceEntity();
         if (sourceEntity != null && VanishAPI.isVanished(sourceEntity) && ConfigManager.vanish().interaction.vibrations)
             cir.setReturnValue(false);
