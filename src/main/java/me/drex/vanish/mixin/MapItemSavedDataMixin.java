372e9a9947e4088c092da62a13f206a04acc9dc9
Drex <nicknamedrex@gmail.com>
2024-01-07 00:25:10 +0100

    Hide vanished players from maps
    resolves #31

---
+++ src/main/java/me/drex/vanish/mixin/MapItemSavedDataMixin.java
@@ -0,0 +1,34 @@
+package me.drex.vanish.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import me.drex.vanish.api.VanishAPI;
+import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(MapItemSavedData.class)
+public abstract class MapItemSavedDataMixin {
+
+    @Shadow
+    protected abstract void removeDecoration(String string);
+
+    @WrapOperation(
+        method = "tickCarriedBy",
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/world/level/saveddata/maps/MapItemSavedData;trackingPosition:Z"
+        )
+    )
+    private boolean vanish_hideFromMap(MapItemSavedData instance, Operation<Boolean> original, @Local MapItemSavedData.HoldingPlayer holdingPlayer) {
+        if (VanishAPI.isVanished(holdingPlayer.player)) {
+            removeDecoration(holdingPlayer.player.getName().getString());
+            return false;
+        } else {
+            return original.call(instance);
+        }
+    }
+
+}
