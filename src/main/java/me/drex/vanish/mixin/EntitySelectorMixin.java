12c2411dc14366c61323d18c9d47f421660b50b3
Drex <nicknamedrex@gmail.com>
2024-06-07 23:44:00 +0200

    Update to 1.21-pre2

--- src/main/java/me/drex/vanish/mixin/EntitySelectorMixin.java
+++ src/main/java/me/drex/vanish/mixin/EntitySelectorMixin.java
@@ -1,26 +1,28 @@
 package me.drex.vanish.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import me.drex.vanish.api.VanishAPI;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.arguments.selector.EntitySelector;
 import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Mixin(EntitySelector.class)
 public abstract class EntitySelectorMixin {
 
-    @Inject(method = "findPlayers", at = @At("RETURN"))
-    public void vanish_removeVanishedPlayers(CommandSourceStack src, CallbackInfoReturnable<List<ServerPlayer>> cir) {
-        List<ServerPlayer> players = cir.getReturnValue();
+    @ModifyReturnValue(method = "findPlayers", at = @At("RETURN"))
+    public List<ServerPlayer> vanish_removeVanishedPlayers(List<ServerPlayer> original, CommandSourceStack src) {
+        List<ServerPlayer> players = new LinkedList<>(original);
         ServerPlayer observer = src.getPlayer();
         if (observer != null) {
             players.removeIf((actor) -> !VanishAPI.canSeePlayer(actor, observer));
         }
+        return players;
     }
 
 }
