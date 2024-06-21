092cbe8db9b0f8013eec00aeeb214af2289e18e8
XXMA16 <81380344+XXMA16@users.noreply.github.com>
2024-06-21 23:22:25 +0300

    Fix containers remaining open by vanished player in certain conditions (#42)
    * Fix containers remaining open by vanished player
    
    * Add changelog
    
    ---------
    
    Co-authored-by: Drex <nicknamedrex@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+- Containers remaining open by vanished players in certain conditions
+
 ## [1.5.6] - 2024-06-20
 ### Added
 - Prevent advancement progress config option
---
+++ src/main/java/me/drex/vanish/mixin/ContainerOpenersCountMixin.java
@@ -0,0 +1,24 @@
+package me.drex.vanish.mixin;
+
+import me.drex.vanish.api.VanishAPI;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.entity.ContainerOpenersCounter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import java.util.function.Predicate;
+
+@Mixin(ContainerOpenersCounter.class)
+public class ContainerOpenersCountMixin {
+    @ModifyArg(
+            method = "getPlayersWithContainerOpen",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/Level;getEntities(Lnet/minecraft/world/level/entity/EntityTypeTest;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;"),
+            index = 2
+    )
+    private Predicate<Player> vanish_excludeVanished(Predicate<Player> predicate) {
+        return predicate.and(player -> !VanishAPI.isVanished(player));
+    }
+}
