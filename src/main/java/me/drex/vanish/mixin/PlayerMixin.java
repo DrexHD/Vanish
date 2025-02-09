790f69f6d25c02d257c6ffee10632e151002fbb2
Drex <nicknamedrex@gmail.com>
2025-02-09 11:05:54 +0100

    Optimize permission lookup

--- src/main/java/me/drex/vanish/mixin/PlayerMixin.java
+++ src/main/java/me/drex/vanish/mixin/PlayerMixin.java
@@ -1,14 +1,23 @@
 package me.drex.vanish.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import me.drex.vanish.api.VanishAPI;
 import me.drex.vanish.config.ConfigManager;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin {
+
+    @Unique
+    private Component vanish_cachedDisplayName = null;
 
     @ModifyReturnValue(method = "isInvulnerableTo", at = @At("RETURN"))
     private boolean vanish_invulnerablePlayers(boolean original) {
@@ -16,6 +25,19 @@ public abstract class PlayerMixin {
             return true;
         }
         return original;
+    }
+
+    @WrapMethod(method = "getDisplayName")
+    public Component vanish_cacheDisplayName(Operation<Component> original) {
+        if (vanish_cachedDisplayName == null) {
+            vanish_cachedDisplayName = original.call();
+        }
+        return vanish_cachedDisplayName;
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    public void vanish_invalidateCache(CallbackInfo ci) {
+        vanish_cachedDisplayName = null;
     }
 
 }
