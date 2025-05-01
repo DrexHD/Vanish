9473d90fed741a9605f3af47c776e1f9c8ef8db1
Drex <nicknamedrex@gmail.com>
2025-05-01 16:18:14 +0200

    Add vanish_on_join event phase

--- src/main/java/me/drex/vanish/compat/ModCompat.java
+++ src/main/java/me/drex/vanish/compat/ModCompat.java
@@ -2,12 +2,15 @@ package me.drex.vanish.compat;
 
 import de.bluecolored.bluemap.api.BlueMapAPI;
 import eu.pb4.styledchat.StyledChatStyles;
+import me.drex.vanish.VanishMod;
 import me.drex.vanish.api.VanishEvents;
 import me.lucko.fabric.api.permissions.v0.Options;
+import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.util.TriState;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import xyz.jpenilla.squaremap.api.*;
 
 public class ModCompat {
@@ -17,14 +20,18 @@ public class ModCompat {
     public static final boolean DYNMAP = FabricLoader.getInstance().isModLoaded("dynmap");
     public static final boolean SQUAREMAP = FabricLoader.getInstance().isModLoaded("squaremap");
     public static final boolean PL3XMAP = FabricLoader.getInstance().isModLoaded("pl3xmap");
+    public static final ResourceLocation VANISH_ON_JOIN = ResourceLocation.fromNamespaceAndPath(VanishMod.MOD_ID, "vanish_on_joín");
     public static boolean blueMapEventsRegistered = false;
 
     public static void init() {
         // Vanilla
         VanishEvents.VANISH_MESSAGE_EVENT.register(serverPlayer -> Component.translatable("multiplayer.player.left", serverPlayer.getDisplayName()).withStyle(ChatFormatting.YELLOW));
         VanishEvents.UN_VANISH_MESSAGE_EVENT.register(serverPlayer -> Component.translatable("multiplayer.player.joined", serverPlayer.getDisplayName()).withStyle(ChatFormatting.YELLOW));
+
         // Util
-        VanishEvents.JOIN_EVENT.register(player -> Options.get(player, "vanish_on_join", Boolean::valueOf).map(TriState::of).orElse(TriState.DEFAULT));
+        // Allow mods to use the default phase to (conditionally) overwrite this feature
+        VanishEvents.JOIN_EVENT.addPhaseOrdering(Event.DEFAULT_PHASE, VANISH_ON_JOIN);
+        VanishEvents.JOIN_EVENT.register(VANISH_ON_JOIN, player -> Options.get(player, "vanish_on_join", Boolean::valueOf).map(TriState::of).orElse(TriState.DEFAULT));
 
         // Styled Chat
         if (STYLED_CHAT) {
